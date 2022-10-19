febdb01802374fadb26dc893b24826fdbee9cf57
BRK-munger <cassandra.kk.munger@icloud.com>
2022-10-19 12:14:42 +0000

    [Add] ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  devise_for :users
+  
   root to: 'homes#top'
+  devise_for :users
+  
+  resources :books, only: [:new, :index, :show, :edit]
+  resources :users, only: [:show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
